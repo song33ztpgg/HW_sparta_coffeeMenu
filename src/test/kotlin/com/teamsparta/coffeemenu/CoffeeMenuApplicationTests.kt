9e93882ce042449cc075b6a5956a6ef2604c0105
song33ztpgg <sasasa7737@gmail.com>
2023-12-28 12:11:48 +0900

    첫번째 커밋

---
+++ src/test/kotlin/com/teamsparta/coffeemenu/CoffeeMenuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.coffeemenu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoffeeMenuApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
